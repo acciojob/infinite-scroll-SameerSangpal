3380a17591786b2c8a6f09efe0fd29c6cc8f8324
Sameer Sangpal <66773934+SameerSangpal@users.noreply.github.com>
2025-07-24 18:45:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,17 @@
 //your code here!
+const list = document.getElementById("infi-list");
+let count = 1;
 
+function addListItems(num){
+	for(let i = 0; i < num; i++){
+		const li = document.createElement("li");
+		li.textContent = `Item ${count++}`;
+		list.appendChild(li);
+	}
+}
+addListItems(10);
+list.addEventListener("scroll", function(){
+	if(list.scrollTop + list.clientHeight >= list.scrollHeight){
+		addListItems(2);
+	}
+});
